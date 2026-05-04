6199a6b56ea7bfc32915de47da03984f5093bfe0
alvinchukwudinwaolie-sketch <alvinchukwudinwaolie@gmail.com>
2026-05-04 14:49:05 +0100

    Add files via upload

---
+++ hyper.js
@@ -0,0 +1,5 @@
+//  const butTon = document.getElementById("button").value;
+function handleLogin() {
+    const butTon = document.getElementById("button");
+    butTon.style.backgroundColor = "red";
+}
